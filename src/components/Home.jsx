088f92ea4a6835dd6da0b5fff5c182eec80bb20a
Madeline Brewer <madelinebrewer95@gmail.com>
2023-01-27 14:50:17 -0700

    run build

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,10 @@ function Home() {
     <div class="home">
     <h1>Hi, i'm Madeline</h1>
     <h3>a software developer</h3>
+    <img src="images/madelinebrew.png" alt="MadelineBrewer" id="profile-pic"></img>
+    <div>
+      <p>Born and raised in Texas, but became a Colorado resident in 2020. I began my web development career in mid 2022, helping a company put some life and style back into their website.</p>
+    </div>
     </div>
   )
 };
